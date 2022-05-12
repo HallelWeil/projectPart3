ce1baf8822d0b0d40e6d0eeeb7e42d4a4e4f2604
orbalmas <101092731+orbalmas@users.noreply.github.com>
2022-05-12 19:54:32 +0300

    Add files via upload
    changes in Zerli Common - cart 
    changes in Zerli Client -shop

---
+++ CatalogController.java
@@ -0,0 +1,22 @@
+package shop;
+
+import java.util.ArrayList;
+import java.util.Locale.Category;
+
+import catalog.*;
+
+public class CatalogController {
+	/**
+	 * change the category, get the category page(default should be 1)
+	 * 
+	 * @param category
+	 * @param newPage
+	 */
+	public void chooseCategory(Category category, int newPage) {
+
+	}
+
+	public void updateCatalog() {
+
+	}
+}
